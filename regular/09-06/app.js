2f31b9739644108a2f9d7d666d555ea576f7d545
harshrath0re <harshavardhan.19jics086@jietjodhpur.ac.in>
2021-06-10 23:59:45 +0530

    Assignment_6

---
+++ regular/09-06/app.js
@@ -0,0 +1,45 @@
+const { Console } = require("console");
+const express = require("express");
+const app =express();
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.get("/", (req,res) =>{
+    res.status(200).send("hello world");
+});
+
+app.get("/about-us", (req,res) =>{
+    res.status(200).send("This is about us page");
+});
+
+app.get("/pokemon", (req,res) =>{
+    axios.get("https://pokeapi.co/api/v2/ability/7/")
+    .then((response) => {
+        console.log(response.data);
+        return res.status(200).send(response.data);
+    })
+    .catch((error) =>{
+        return res.status(500).send("error");
+    });
+});
+
+app.post("/login", (req,res) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+    // req.body.password("123456789")
+    const pass = "123456789";
+    if(password === pass){
+      res.status(200).send("correct password");
+    }else {
+        res.status(401).send("password incorrect");
+    }
+});
+
+const PORT =7000;
+
+app.listen(PORT, () => {
+    console.log(`MY server is running on port 7000`);
+});
