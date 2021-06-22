f48b687fe71c84c0635da6b32af14d42400d6b3a
harshrath0re <harshavardhan.19jics086@jietjodhpur.ac.in>
2021-06-22 08:43:47 +0530

    Task React Application

--- regular/10-06/app.js
+++ regular/10-06/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const userRoute = require("./routes/userRoutes");
+const userRoute = require("./routes/UserRoute");
+const cors = require("cors");
+const taskRoute = require("./routes/TaskRoute");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect("mongodb://localhost:27017/jiet", {
@@ -19,8 +22,9 @@ mongoose
   });
 
 app.use(userRoute);
+app.use("/task", taskRoute);
 
-const PORT = 9009;
+const PORT = 7000;
 
 app.listen(PORT, () => {
   console.log(`The server is running on port: ${PORT}`);
